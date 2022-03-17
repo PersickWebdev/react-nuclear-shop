46d47737317e3ec3b0374ff7ad913c541f519ecc
Vlad <vladdev461@gmail.com>
2022-03-17 19:05:24 +0200

    styles(project): grid template created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './App.module.scss';
+import { Header, Footer } from './components';
 
 export const App = () => {
     return (
-        <div className="application">
-            App
+        <div className={styles['application']}>
+            <Header/>
+            <main className={styles['application__main']}>
+                Main
+            </main>
+            <Footer/>
         </div>
     );
 }
-
-export default App;
