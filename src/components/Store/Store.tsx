4789e02636324bf5bd5a8806330ac8cd3b97a608
Vlad <vladdev461@gmail.com>
2022-03-20 03:03:40 +0200

    feature(Store): "currentCategoryProducts" passed to "Store"

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import styles from './Store.module.scss';
+import { IProduct } from '../../types';
 
-const Store = () => {
+interface IStore {
+    currentCategoryProducts: IProduct[];
+}
+
+const Store = ({ currentCategoryProducts }: IStore) => {
+    console.log('Store - currentCategoryProducts: ', currentCategoryProducts);
+
     return (
         <div className={styles['store']}>
             <ul className={styles['store__list']}>
--- src/pages/StorePage/StorePage.tsx
+++ src/pages/StorePage/StorePage.tsx
@@ -55,7 +55,9 @@ const StorePage = () => {
                 setCurrentCategory={setCurrentCategory}
             />
             <div className={styles['container']}>
-                <Store />
+                <Store
+                    currentCategoryProducts={currentCategoryProducts}
+                />
             </div>
         </div>
     );
